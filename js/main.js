d665f9ad3d8dfde0dcb2dbc119e4c7f4dd5b92b3
Allison Logan <agcandreva@yahoo.com>
2015-04-06 21:56:12 -0400

    adds backstretch and images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready( function () {
 
+  $('header').backstretch([
+    "img/chicken.jpg",
+    "img/breadcheese2.jpg",
+    "img/fish.jpg",
+    "img/pighead.jpg",
+    "img/octopussalad.jpg"
+  ], {duration: 3000, fade: 750});
+  
+
 });
